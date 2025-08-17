2b38398390ce05823bb3037472586b032e0959cf
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-08-17 10:55:22 +0530

    Day-3

---
+++ DuplicateNumber.java
@@ -0,0 +1,22 @@
+public class DuplicateNumber {
+    public static int findDuplicateNumber(int[] arr){
+        int hare=arr[0];
+        int tortoise=arr[0];
+        do{
+            tortoise=arr[tortoise];
+            hare=arr[arr[hare]];
+        }
+        while(tortoise!=hare);
+        tortoise=arr[0];
+        while (tortoise!=hare){
+            tortoise=arr[tortoise];
+            hare=arr[hare];
+        }
+        return tortoise;
+    }
+    public static void main(String[] args){
+        int[] arr={3,1,4,2,2};
+        int duplicate=findDuplicateNumber(arr);
+        System.out.println(duplicate);
+    }
+}
